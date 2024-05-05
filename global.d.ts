a1ba65661d38b575d81ab6337985e5c54029dc35
0xWaleed <imwaleed@outlook.sa>
2024-05-05 21:18:36 +0300

    style: fix indentation 💄

--- global.d.ts
+++ global.d.ts
@@ -1,43 +1,44 @@
-export {}
+export { }
 
-declare global{
-interface User {
-	id: string;
-	username: string;
+declare global {
+	interface User {
+		id: string;
+		username: string;
+	}
+
+	interface Member {
+		user: User;
+		code?: string;
+		invitedBy?: string;
+	}
+
+	interface ActualMemberDTO {
+		user: User;
+	}
+
+
+	interface MemberDTO {
+		source_invite_code?: string;
+		join_source_type?: number;
+		inviter_id?: string;
+		member: ActualMemberDTO;
+	}
+
+	interface MemberSearchDTO {
+		guild_id: string;
+		members: MemberDTO[];
+		page_result_count: number;
+		total_result_count: number;
+	}
+
+	interface InviteDTO {
+		code: string;
+		inviter?: User;
+	}
+
+
+	interface Invite {
+		code: string;
+		user?: User;
+	}
 }
-
-interface Member {
-	user: User;
-	code?: string;
-	invitedBy?: string;
-}
-
-interface ActualMemberDTO {
-	user: User;
-}
-
-
-interface MemberDTO {
-	source_invite_code?: string;
-	join_source_type?: number;
-	inviter_id?: string;
-	member: ActualMemberDTO;
-}
-
-interface MemberSearchDTO {
-	guild_id: string;
-	members: MemberDTO[];
-	page_result_count: number;
-	total_result_count: number;
-}
-
-interface InviteDTO {
-	code: string;
-	inviter?: User;
-}
-
-
-interface Invite {
-	code: string;
-	user?: User;
-}}
